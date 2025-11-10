7ba276811c72d3745bcc1222b007c298a1011c35
Hyeji <hyeji09261@tukorea.ac.kr>
2025-11-10 15:35:26 +0900

    main 수정

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from pico2d import*
 import game_framework
 import logo_mode
-import play_mode as start_mode
+import logo_mode as start_mode
 
 open_canvas (1000,550)
-logo_mode.init()
-while logo_mode.running:
-    logo_mode.handle_events()
-    logo_mode.update()
-    logo_mode.draw()
-    delay(0.01)
-logo_mode.finish()
 
 game_framework.run(start_mode)
 close_canvas()
