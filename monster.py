c2c1f7aded8bd70d82ebbbc68e9c174683886dbc
Hyeji <hyeji09261@tukorea.ac.kr>
2025-11-12 22:39:41 +0900

    몬스터 프레임 수치 수정

--- monster.py
+++ monster.py
@@ -4,19 +4,21 @@ import game_framework
 
 
 PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
-RUN_SPEED_KMPH = 25.0 # Km / Hour
+RUN_SPEED_KMPH = 20.0 # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 
 SHEET_PATH = '사진수집/monster/주황버섯.png'
-W, H    = 64, 64      # 프레임 크기
-PITCH   = 64          # 다음 프레임까지 이동량(여백 없으면 W와 동일)
-START_X = 0           # 첫 프레임 left
-START_Y = 0           # 프레임 줄 bottom
-FRAMES  = 4           # 달리기 프레임 수
-FPS     = 8.0         # 애니 속도(프레임/초)
+STARY_X = 235
+START_Y = 260          # 프레임 줄 bottom
+FRAMES  = 3           # 달리기 프레임 수
+FPS     = 6.0        # 애니 속도(프레임/초)
+
+FRAME_LEFT  = [235, 381, 490]  # ← 2번째 프레임(left)을 몇 픽셀 오른쪽으로 밀기
+FRAME_WIDTH = [145, 113, 145]    # ← 2번째 프레임의 폭을 조금 줄여 가장자리 ‘번짐/붙음’ 제거
+FRAME_HEIGHT = 120            # 높이는 동일하면 한 값만 둬도 됨
 
 class Mushroom:
     def __init__(self, x, y, field=None):
@@ -24,14 +26,14 @@ class Mushroom:
         self.dir = random.choice([-1, 1])   # -1: 왼쪽, 1: 오른쪽
         self.frame = 0
         self.acc = 0.0
-        self.scale = 0.9
-        self.foot_adj = 0
+        self.scale = 0.8
+        self.foot_adj = 45
 
         self.image = load_image(SHEET_PATH)
         self.camera = None
         if field: self.set_camera(field)
 
-        # 월드 경계 (기본값 — playmode에서 맵 폭으로 덮어써줄 것)
+        # 월드 경계
         self.world_min_x = 0
         self.world_max_x = 5000
 
@@ -75,16 +77,17 @@ class Mushroom:
     def draw(self):
         sx, sy = self.screen_xy()
 
-        DW, DH = int(W * self.scale), int(H * self.scale)
-        y = sy - (DH - H)//2 - self.foot_adj
+        i = self.frame % FRAMES
+        left = FRAME_LEFT[i]
+        width = FRAME_WIDTH[i]
+        height = FRAME_HEIGHT
 
-        left = START_X + (self.frame % FRAMES) * PITCH
-        bottom = START_Y
+        # 스케일 적용
+        DW, DH = int(width * self.scale), int(height * self.scale)
+        y = sy - (DH - height) // 2 - self.foot_adj
 
-        # 기본 시트가 '왼쪽'을 보게 그려져 있다고 가정
         flip = '' if self.dir == -1 else 'h'
-        self.image.clip_composite_draw(left, bottom, W, H, 0, flip, sx, y, DW, DH)
-
+        self.image.clip_composite_draw(left, START_Y, width, height, 0, flip, sx, y, DW, DH)
     # 충돌 박스(원하면 숫자만 다듬어도 됨)
     def get_bb(self):
         hw = int(W * self.scale) // 2 - 6
